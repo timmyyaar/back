d76b660deb988feaf7bfc296d8a91050254464b7
Jewgenij Larkin <jewglark@Jewgenijs-MacBook-Pro.local>
2024-02-13 17:38:59 +0100

    Adde promo router

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const careersRouter = require('../modules/career');
 const localesRouter = require('../modules/locales');
 const tasksRouter = require('../modules/tasks');
+const promoRouter = require('../modules/promo');
 
 const router = express.Router();
 
 router
 	.use(careersRouter)
 	.use(localesRouter)
+	.use(promoRouter)
 	.use(tasksRouter);
 
 module.exports = router;
