3409f2aee50873f256b7831b2112474620fcec39
Jewgenij Larkin <jewglark@jewgenijs-mbp.home>
2024-02-15 19:21:24 +0100

    Remove order router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ const careersRouter = require('../modules/career');
 const localesRouter = require('../modules/locales');
 const tasksRouter = require('../modules/tasks');
 const promoRouter = require('../modules/promo');
-const ordersRouter = require('../modules/orders');
 const giftRouter = require('../modules/gift');
 
 const router = express.Router();
@@ -13,7 +12,6 @@ router
 	.use(careersRouter)
 	.use(localesRouter)
 	.use(promoRouter)
-	.use(ordersRouter)
 	.use(giftRouter)
 	.use(tasksRouter);
 
