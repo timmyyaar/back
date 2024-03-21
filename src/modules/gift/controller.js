b34c96a98c9f5575772b88563bdc317247f75972
r.boikov <r.boikov@wsc-sports.com>
2024-03-21 21:57:39 +0200

    Fixed field names in gifts controller

--- src/modules/gift/controller.js
+++ src/modules/gift/controller.js
@@ -1,81 +1,81 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const env = require('../../helpers/environments');
+const env = require("../../helpers/environments");
 
 const GiftController = () => {
-	const getClient = () => {
-		const POSTGRES_URL = env.getEnvironment('POSTGRES_URL');
-		const client = new Client({
-			connectionString: `${POSTGRES_URL}?sslmode=require`,
-		});
+  const getClient = () => {
+    const POSTGRES_URL = env.getEnvironment("POSTGRES_URL");
+    const client = new Client({
+      connectionString: `${POSTGRES_URL}?sslmode=require`,
+    });
 
-		return client;
-	};
+    return client;
+  };
 
-	const getGift = async (req, res) => {
-		const client = getClient();
+  const getGift = async (req, res) => {
+    const client = getClient();
 
-		try {
-			await client.connect();
-			const result = await client.query('SELECT * FROM gift');
+    try {
+      await client.connect();
+      const result = await client.query("SELECT * FROM gift");
 
-			res.json({ gift: result.rows });
-		} catch (error) {
-			res.status(500).json({ error });
-		} finally {
-			await client.end();
-		}
-	};
+      res.json({ gift: result.rows });
+    } catch (error) {
+      res.status(500).json({ error });
+    } finally {
+      await client.end();
+    }
+  };
 
-	const createGift = async (req, res) => {
-		const client = getClient();
+  const createGift = async (req, res) => {
+    const client = getClient();
 
-		try {
-			const { fEmail, fPhone, comment } = req.body;
-			if (fEmail && fPhone && comment) {
-				await client.connect();
+    try {
+      const { email, phone, comment } = req.body;
+      if (email && phone && comment) {
+        await client.connect();
 
-				const result = await client.query(
-					'INSERT INTO gift(fEmail, fPhone, comment) VALUES($1, $2, $3) RETURNING *',
-					[fEmail, fPhone, comment],
-				);
+        const result = await client.query(
+          "INSERT INTO gift(email, phone, comment) VALUES($1, $2, $3) RETURNING *",
+          [email, phone, comment]
+        );
 
-				res.status(200).json({ gift: result.rows[0] });
-			} else {
-				res.status(422).json({ message: 'Unprocessable Entity' });
-			}
-		} catch (error) {
-			res.status(500).json({ error });
-		} finally {
-			await client.end();
-		}
-	};
+        res.status(200).json({ gift: result.rows[0] });
+      } else {
+        res.status(422).json({ message: "Unprocessable Entity" });
+      }
+    } catch (error) {
+      res.status(500).json({ error });
+    } finally {
+      await client.end();
+    }
+  };
 
-	const deleteGift = async (req, res) => {
-		const client = getClient();
+  const deleteGift = async (req, res) => {
+    const client = getClient();
 
-		try {
-			const { id } = req.body;
-			await client.connect();
+    try {
+      const { id } = req.body;
+      await client.connect();
 
-			const result = await client.query(
-				'DELETE FROM gift WHERE id = $1 RETURNING *',
-				[id],
-			);
+      const result = await client.query(
+        "DELETE FROM gift WHERE id = $1 RETURNING *",
+        [id]
+      );
 
-			res.status(200).json({ message: 'Gift deleted', gift: result.rows });
-		} catch (error) {
-			res.status(500).json({ error });
-		} finally {
-			await client.end();
-		}
-	};
+      res.status(200).json({ message: "Gift deleted", gift: result.rows });
+    } catch (error) {
+      res.status(500).json({ error });
+    } finally {
+      await client.end();
+    }
+  };
 
-	return ({
-		getGift,
-		createGift,
-		deleteGift,
-	});
+  return {
+    getGift,
+    createGift,
+    deleteGift,
+  };
 };
 
 module.exports = GiftController();
