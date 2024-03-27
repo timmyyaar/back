6bb8a7d2008aa62df76ce60710a139d883442722
r.boikov <r.boikov@wsc-sports.com>
2024-03-27 23:33:10 +0200

    Order update and some filters and other features

--- src/modules/order/index.js
+++ src/modules/order/index.js
@@ -9,6 +9,7 @@ const orderRouter = new Router();
 orderRouter
   .get("/order", verifyToken, orderController.getOrder)
   .post("/order", orderController.createOrder)
+  .put("/order/:id", verifyToken, orderController.updateOrder)
   .delete("/order/:id", verifyToken, orderController.deleteOrder)
   .patch("/order/:id/:cleanerId", verifyToken, orderController.assignOrder)
   .patch(
