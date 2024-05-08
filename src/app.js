72b53904743c728538d4918aa5bc6f496d3f4e44
r.boikov <r.boikov@wsc-sports.com>
2024-05-08 18:02:19 +0300

    CORS optimization

--- src/app.js
+++ src/app.js
@@ -8,17 +8,41 @@ const { stripeWebhook } = require("./webhooks/stripe-webhook");
 
 require("dotenv").config();
 
+const env = require("./helpers/environments");
+
+const getOrigin = () => {
+  const appMode = env.getEnvironment("MODE");
+
+  switch (appMode) {
+    case "prod":
+      return "https://www.takeutime.pl/";
+    case "staging":
+      return "https://www.staging.takeutime.pl/";
+    default:
+      return true;
+  }
+};
+
 const router = require("./router");
 
 const app = express();
 
 app.use(logger("dev"));
-app.post("/stripe-webhook", express.raw({ type: "application/json" }), stripeWebhook);
+app.post(
+  "/stripe-webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhook
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: getOrigin(),
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
