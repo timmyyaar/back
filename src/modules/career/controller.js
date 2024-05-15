61b654e93bf036e534fdd05e20f672300bd2fda4
r.boikov <r.boikov@wsc-sports.com>
2024-05-15 19:04:56 +0300

    Career telegram notifications

--- src/modules/career/controller.js
+++ src/modules/career/controller.js
@@ -1,8 +1,20 @@
 const pool = require("../../db/pool");
 
-const env = require("../../helpers/environments");
-
 const constants = require("../../constants");
+
+const CREATED_ORDERS_CHANNEL_ID = "-1002017671793";
+
+const sendTelegramMessage = async () => {
+  await fetch(
+    `https://api.telegram.org/bot${env.getEnvironment(
+      "TELEGRAM_BOT_ID"
+    )}/sendMessage?` +
+      new URLSearchParams({
+        chat_id: CREATED_ORDERS_CHANNEL_ID,
+        text: "New career application!",
+      })
+  );
+};
 
 const CareersController = () => {
   const getCareers = async (req, res) => {
@@ -24,18 +36,21 @@ const CareersController = () => {
   const createNewCareers = async (req, res) => {
     try {
       const { name, phone, email, about = "", referralCode = null } = req.body;
+
       if (name && phone && email) {
         const result = await pool.query(
           "INSERT INTO careers(name, phone, email, about, referral_code) VALUES($1, $2, $3, $4, $5) RETURNING *",
           [name, phone, email, about, referralCode]
         );
 
-        res.status(200).json({ careers: result.rows[0] });
+        await sendTelegramMessage();
+
+        return res.status(200).json(result.rows[0]);
       } else {
-        res.status(422).json({ message: "Unprocessable Entity" });
+        return res.status(422).json({ message: "Unprocessable Entity" });
       }
     } catch (error) {
-      res.status(500).json({ error });
+      return res.status(500).json({ error });
     }
   };
 
